7280ba8a97f00585ab37851ce601c698b853f53d
Vita Kostiuk <vitagrebennik@gmail.com>
2022-05-09 15:09:11 +0300

    add component ContactListItem

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ContactListItem } from '../ContactListItem';
 
 import s from './ContactList.module.css';
 
 export const ContactList = ({ contacts, onDeleteContact }) => (
   <ul className={s.List}>
-    {contacts.map(({ id, name, number }) => (
-      <li key={id} className={s.Item}>
-        {name}
-        <span className={s.Number}>{number}</span>
-        <button
-          type="button"
-          className={s.Btn}
-          onClick={() => onDeleteContact(id)}
-        >
-          Delete
-        </button>
-      </li>
-    ))}
+    <ContactListItem contacts={contacts} onDeleteContact={onDeleteContact} />
   </ul>
 );
 
 ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  contacts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
   onDeleteContact: PropTypes.func.isRequired,
 };
