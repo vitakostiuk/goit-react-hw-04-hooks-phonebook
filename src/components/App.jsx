6b06dbe162240b600ee1c2501df28bfec3ac49c2
Vita Kostiuk <vitagrebennik@gmail.com>
2022-05-17 17:27:35 +0300

    fixed save to locale storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import s from '../components/App.module.css';
 
 export const App = () => {
   const [contacts, setContacts] = useState(
-    JSON.parse(localStorage.getItem('contacts')) ?? []
+    () => JSON.parse(localStorage.getItem('contacts')) ?? []
   );
   const [filter, setFilter] = useState('');
 
